1a1a33b55a680eb4407a6fcc7158b182a7f88dce
csl <chenshuailin66@163.com>
2020-12-29 11:13:20 +0800

    20201229本地执行基本完成调试，解决执行完成后排放任务混乱和写回设备MongoDB混乱的问题

--- app/ApplicationAndTask/Execute/DeviceManager.go
+++ app/ApplicationAndTask/Execute/DeviceManager.go
@@ -19,24 +19,22 @@ var deviceManagerVars DeviceManagerVars
 
 type deviceManage struct {
 	lock     *sync.Mutex
-	taskList []*TaskExecuteUnit
+	taskList map[string]*TaskExecuteUnit
 }
 type devicemanager struct {
 	deviceMap map[string]deviceManage
 }
 
 //从任务列表中拿出某个TaskExecuteUnit
-func popTask(index int, deviceid string) *TaskExecuteUnit {
-	task := deviceManagerVars.m.deviceMap[deviceid].taskList[index]
-	tl := append(deviceManagerVars.m.deviceMap[deviceid].taskList[:index], deviceManagerVars.m.deviceMap[deviceid].taskList[index+1:]...)
-	l := deviceManagerVars.m.deviceMap[deviceid].lock
-	deviceManagerVars.m.deviceMap[deviceid] = deviceManage{lock: l, taskList: tl}
-	return task
+func popTask(task *TaskExecuteUnit, deviceid string) {
+	//deviceManagerVars.mutex.Lock()
+	delete(deviceManagerVars.m.deviceMap[deviceid].taskList, task.Id.Hex())
+	//deviceManagerVars.mutex.Unlock()
 }
 
 //从任务列表中抛出执行失败的TaskExecuteUnit
-func failTask(index int, deviceid string) {
-	task := popTask(index, deviceid)
+func failTask(task *TaskExecuteUnit, deviceid string) {
+	popTask(task, deviceid)
 	task.Result = result{ExecResult: domain.Fail}
 	task.State = executed
 }
@@ -55,7 +53,7 @@ func (m *devicemanager) LoadAllDevice() {
 	var deviceList, _ = db.GetDeviceRepos().SelectAll()
 	deviceManagerVars.mutex.Lock()
 	for i := 0; i < len(deviceList); i++ {
-		deviceManagerVars.m.deviceMap[deviceList[i].Id.Hex()] = deviceManage{lock: new(sync.Mutex), taskList: make([]*TaskExecuteUnit, 0)}
+		deviceManagerVars.m.deviceMap[deviceList[i].Id.Hex()] = deviceManage{lock: new(sync.Mutex), taskList: make(map[string]*TaskExecuteUnit, 0)}
 	}
 	deviceManagerVars.mutex.Unlock()
 }
@@ -67,10 +65,6 @@ func (m *devicemanager) ReloadDevices() {
 	newDeviceMap := make(map[string]deviceManage, 0)
 	for i := 0; i < len(deviceList); i++ {
 		newDeviceMap[deviceList[i].Id.Hex()] = deviceManagerVars.m.deviceMap[deviceList[i].Id.Hex()]
-		for len(deviceManagerVars.m.deviceMap[deviceList[i].Id.Hex()].taskList) > 0 {
-			failTask(i, deviceList[0].Id.Hex())
-		}
-		delete(deviceManagerVars.m.deviceMap, deviceList[i].Id.Hex())
 	}
 	deviceManagerVars.m.deviceMap = newDeviceMap
 	deviceManagerVars.mutex.Unlock()
@@ -83,15 +77,15 @@ func (m *devicemanager) AddDevice(device domain.Device) {
 		deviceManagerVars.mutex.Unlock()
 		return
 	}
-	deviceManagerVars.m.deviceMap[device.Id.Hex()] = deviceManage{lock: new(sync.Mutex), taskList: make([]*TaskExecuteUnit, 0)}
+	deviceManagerVars.m.deviceMap[device.Id.Hex()] = deviceManage{lock: new(sync.Mutex), taskList: make(map[string]*TaskExecuteUnit, 0)}
 	deviceManagerVars.mutex.Unlock()
 }
 
 //删除某个设备
 func (m *devicemanager) RemoveDevice(id string) {
 	deviceManagerVars.mutex.Lock()
-	for len(deviceManagerVars.m.deviceMap[id].taskList) > 0 {
-		failTask(0, id)
+	for _, v := range deviceManagerVars.m.deviceMap[id].taskList {
+		failTask(v, id)
 	}
 	delete(deviceManagerVars.m.deviceMap, id)
 	deviceManagerVars.mutex.Unlock()
@@ -101,6 +95,7 @@ func (m *devicemanager) RemoveDevice(id string) {
 func (m *devicemanager) ExecuteTasks(deviceid string, tasks []*TaskExecuteUnit) {
 	//此时首先锁定设备，因为这时要对设备内部的数组进行修改，还要根据时间限制进行排序，不能冲突
 	deviceManagerVars.mutex.Lock()
+	//找不到对应的设备，直接Fail掉相应Task
 	if _, ok := deviceManagerVars.m.deviceMap[deviceid]; !ok {
 		for i := 0; i < len(tasks); i++ {
 			tasks[i].Result = result{ExecResult: domain.Fail}
@@ -110,9 +105,9 @@ func (m *devicemanager) ExecuteTasks(deviceid string, tasks []*TaskExecuteUnit) 
 		return
 	}
 	deviceManagerVars.m.deviceMap[deviceid].lock.Lock()
-	newTaskList := append(deviceManagerVars.m.deviceMap[deviceid].taskList, tasks...)
-	lock := deviceManagerVars.m.deviceMap[deviceid].lock
-	deviceManagerVars.m.deviceMap[deviceid] = deviceManage{lock: lock, taskList: newTaskList}
+	for i := 0; i < len(tasks); i++ {
+		deviceManagerVars.m.deviceMap[deviceid].taskList[tasks[i].Id.Hex()] = tasks[i]
+	}
 	m.allocateTasksForDevice(deviceid)
 	deviceManagerVars.m.deviceMap[deviceid].lock.Unlock()
 	deviceManagerVars.mutex.Unlock()
@@ -127,19 +122,34 @@ func GetDeviceManager() DeviceManager {
 }
 
 //为某个设备分配任务
-func (dm deviceManage) Len() int      { return len(dm.taskList) }
-func (dm deviceManage) Swap(i, j int) { dm.taskList[i], dm.taskList[j] = dm.taskList[j], dm.taskList[i] }
-func (dm deviceManage) Less(i, j int) bool {
-	return dm.taskList[i].Task.Command.TimeLimit < dm.taskList[j].Task.Command.TimeLimit
+//排序相关功能
+type Pair struct {
+	Id   string
+	Task *TaskExecuteUnit
+}
+type pairList []Pair
+
+func (p pairList) Swap(i, j int) { p[i], p[j] = p[j], p[i] }
+func (p pairList) Len() int      { return len(p) }
+func (p pairList) Less(i, j int) bool {
+	return p[i].Task.Task.Command.TimeLimit < p[j].Task.Task.Command.TimeLimit
+}
+func sortTaskList(taskList map[string]*TaskExecuteUnit) map[string]*TaskExecuteUnit {
+	p := make(pairList, 0)
+	for k, v := range taskList {
+		p = append(p, Pair{Id: k, Task: v})
+	}
+	sort.Sort(p)
+	newTaskList := make(map[string]*TaskExecuteUnit, 0)
+	for i := 0; i < len(p); i++ {
+		newTaskList[p[i].Id] = p[i].Task
+	}
+	return newTaskList
 }
 func (m *devicemanager) allocateTasksForDevice(deviceid string) {
 	//定义如何执行任务,这里如果条件允许那么只有分配的CPU系数会不同
-	execTask := func(device *domain.Device, cpu_count int64, cpu_unit int64, index int) {
-		task := popTask(index, deviceid)
-		task.Cpu = cpu_unit * cpu_count
-		task.Memory = task.Task.Command.MemoryRequest
-		task.Disk = task.Task.Command.DiskRequest
-		task.NetRate = task.Task.Command.Size
+	execTask := func(task *TaskExecuteUnit, device *domain.Device) {
+		popTask(task, deviceid)
 		device.CpuUsed += task.Cpu
 		device.MemoryUsed += task.Memory
 		device.DiskUsed += task.Disk
@@ -150,6 +160,7 @@ func (m *devicemanager) allocateTasksForDevice(deviceid string) {
 			time.Sleep(time.Duration(task.Task.Command.CPURequest/task.Cpu) * time.Second)
 			task.Result = result{ExecTime: time.Now(), ExecPlace: "LOCAL", EnergyUsed: task.Task.Command.CPURequest / task.Cpu, ExecResult: domain.OK}
 			task.State = executed
+			deviceManagerVars.m.deviceMap[device.Id.Hex()].lock.Lock()
 			newdevice, _ := db.GetDeviceRepos().Select(device.Id.Hex())
 			device = &newdevice
 			device.CpuUsed -= task.Cpu
@@ -157,23 +168,24 @@ func (m *devicemanager) allocateTasksForDevice(deviceid string) {
 			device.DiskUsed -= task.Disk
 			device.NetRateUsed -= task.NetRate
 			db.GetDeviceRepos().Update(device)
+			deviceManagerVars.m.deviceMap[device.Id.Hex()].lock.Unlock()
 		}()
 	}
 	//按照完成时间限制排队,先执行完成时间限制高的,也就是TimeLimit小的
-	sort.Sort(deviceManagerVars.m.deviceMap[deviceid])
+	deviceManagerVars.m.deviceMap[deviceid] =
+		deviceManage{lock: deviceManagerVars.m.deviceMap[deviceid].lock, taskList: sortTaskList(deviceManagerVars.m.deviceMap[deviceid].taskList)}
 	//为其分配CPU和内存，先给紧急任务分
-	for i := 0; i < len(deviceManagerVars.m.deviceMap[deviceid].taskList); i++ {
+	for _, task := range deviceManagerVars.m.deviceMap[deviceid].taskList {
 		//如果找不到设备，执行失败
 		device, err := db.GetDeviceRepos().Select(deviceid)
 		if err != nil {
-			failTask(i, deviceid)
+			failTask(task, deviceid)
 		}
-		task := deviceManagerVars.m.deviceMap[deviceid].taskList[i]
 		command := task.Task.Command
 		//如果设备的全部资源也不够能完成这些任务，直接抛出执行失败
 		if (command.TimeLimit > 0 && command.CPURequest/device.Cpu > command.TimeLimit) ||
 			command.DiskRequest > device.Disk || command.MemoryRequest > device.Memory || command.Size > device.NetRate {
-			failTask(i, deviceid)
+			failTask(task, deviceid)
 		}
 		avail_cpu := device.Cpu - device.CpuUsed
 		avail_memory := device.Memory - device.MemoryUsed
@@ -184,19 +196,15 @@ func (m *devicemanager) allocateTasksForDevice(deviceid string) {
 			cpu_unit := device.Cpu / 20
 			cpu_count := int64(1)
 			for ; cpu_count*cpu_unit <= avail_cpu; cpu_count++ {
-				if task.Task.Command.TimeLimit == 0 {
-					execTask(&device, cpu_count, cpu_unit, i)
-					break
-				}
-				if task.Task.Command.CPURequest/(cpu_count*cpu_unit) < task.Task.Command.TimeLimit {
-					execTask(&device, cpu_count, cpu_unit, i)
+				if task.Task.Command.TimeLimit == 0 || (task.Task.Command.CPURequest/(cpu_count*cpu_unit) < task.Task.Command.TimeLimit) {
+					task.Cpu = cpu_unit * cpu_count
+					task.Memory = task.Task.Command.MemoryRequest
+					task.Disk = task.Task.Command.DiskRequest
+					task.NetRate = task.Task.Command.Size
+					execTask(task, &device)
 					break
 				}
 			}
-			task.Cpu = cpu_unit * cpu_count
-			task.Memory = task.Task.Command.MemoryRequest
-			task.Disk = task.Task.Command.DiskRequest
-			task.NetRate = task.Task.Command.Size
 		} else {
 			continue
 		}
